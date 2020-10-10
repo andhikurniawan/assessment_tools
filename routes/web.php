44bf955638706ca7531a15f27aef83870b70b2fa
Ekky Novriza Alam <ekkynovriza@gmail.com>
2020-10-10 09:19:30 +0700

    add new config

--- routes/web.php
+++ routes/web.php
@@ -17,15 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-
 Auth::routes(['verify' => true]);
 
 Route::get('/home', 'HomeController@index')->middleware('verified');
