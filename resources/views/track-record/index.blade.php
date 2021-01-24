79e62a4b95fc550a4bb875c8092fcdeafbc1df2a
Adhitya Kusumadinatha <adhityasantika@yahoo.com>
2021-01-24 19:45:33 +0700

    Modified : Rename Ubah button to Simpan button in Modal

--- resources/views/track-record/index.blade.php
+++ resources/views/track-record/index.blade.php
@@ -137,7 +137,7 @@
                         </div>
                         <div class="modal-footer">
                             <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
-                            <button type="submit" class="btn btn-primary">Ubah</button>
+                            <button type="submit" class="btn btn-primary">Simpan</button>
                         </div>
                         </form>
                     </div>
